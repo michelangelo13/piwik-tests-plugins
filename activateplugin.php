006e90ca7b9f8ef3217de9f808d78a9d85b81a70
Piwik Team <hello@piwik.org>
2013-09-26 13:54:04 +1200

    Create activateplugin.php

---
+++ activateplugin.php
@@ -0,0 +1,40 @@
+<?php
+
+if (empty($argv[1])) {
+    echo "No plugin name specified.\nTry 'activateplugin.php {PluginName}'\n";
+    exit(1);
+}
+
+$pluginName = $argv[1];
+
+define('PIWIK_DOCUMENT_ROOT', dirname(__FILE__) == '/' ? '' : dirname(__FILE__));
+define('PIWIK_INCLUDE_PATH', PIWIK_DOCUMENT_ROOT);
+define('PIWIK_USER_PATH', PIWIK_DOCUMENT_ROOT);
+
+require_once PIWIK_DOCUMENT_ROOT . '/vendor/autoload.php';
+require_once PIWIK_DOCUMENT_ROOT . '/core/Loader.php';
+require_once PIWIK_DOCUMENT_ROOT . '/core/functions.php';
+require_once PIWIK_INCLUDE_PATH . '/libs/upgradephp/upgrade.php';
+
+use \Piwik\Config;
+use \Piwik\Filesystem;
+
+$plugins = Config::getInstance()->Plugins['Plugins'];
+if (!in_array($pluginName, $plugins)) {
+    $plugins[] = $pluginName;
+    $section = Config::getInstance()->Plugins;
+    $section['Plugins'] = $plugins;
+    Config::getInstance()->Plugins = $section;
+}
+
+$pluginsInstalled = Config::getInstance()->PluginsInstalled['PluginsInstalled'];
+
+if (!in_array($pluginName, $pluginsInstalled)) {
+    $pluginsInstalled[] = $pluginName;
+    $section = Config::getInstance()->PluginsInstalled;
+    $section['PluginsInstalled'] = $pluginsInstalled;
+    Config::getInstance()->PluginsInstalled = $section;
+}
+
+Config::getInstance()->forceSave();
+Filesystem::deleteAllCacheOnUpdate();
