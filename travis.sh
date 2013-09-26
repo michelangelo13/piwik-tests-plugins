55bc4a325b5cbdf8b6ae9fdae32dd207822765d0
Thomas Steur <tsteur@users.noreply.github.com>
2013-09-27 10:49:32 +1200

    include travis build id in pingback

--- travis.sh
+++ travis.sh
@@ -18,15 +18,15 @@ HAS_FATAL=$?
 
 if [ 0 == $HAS_FATAL ]; then
    echo "FATAL";
-   curl "http://plugins.piwik.org/post-tests-hook/$TEST_PIWIK_VERISON/$TEST_PLUGIN_NAME/$TEST_PLUGIN_VERSION/fatal?secret=$API_HOOK_SECRET"
+   curl "http://plugins.piwik.org/post-tests-hook/$TEST_PIWIK_VERISON/$TEST_PLUGIN_NAME/$TEST_PLUGIN_VERSION/fatal?secret=$API_HOOK_SECRET&travisBuildId=$TRAVIS_BUILD_ID"
    exit 1;
 elif [ 0 == $BREAKS_BUILD_PLUGIN_TESTS ] && [ 0 == $BREAKS_BUILD_CORE_TESTS ] && [ 0 == $BREAKS_BUILD_INTEGRATION_TESTS ]; then
    echo "SUCCESS"
-   curl "http://plugins.piwik.org/post-tests-hook/$TEST_PIWIK_VERISON/$TEST_PLUGIN_NAME/$TEST_PLUGIN_VERSION/success?secret=$API_HOOK_SECRET"
+   curl "http://plugins.piwik.org/post-tests-hook/$TEST_PIWIK_VERISON/$TEST_PLUGIN_NAME/$TEST_PLUGIN_VERSION/success?secret=$API_HOOK_SECRET&travisBuildId=$TRAVIS_BUILD_ID"
    exit 0;
 else
    echo "FAILURE";
-   curl "http://plugins.piwik.org/post-tests-hook/$TEST_PIWIK_VERISON/$TEST_PLUGIN_NAME/$TEST_PLUGIN_VERSION/failure?secret=$API_HOOK_SECRET"
+   curl "http://plugins.piwik.org/post-tests-hook/$TEST_PIWIK_VERISON/$TEST_PLUGIN_NAME/$TEST_PLUGIN_VERSION/failure?secret=$API_HOOK_SECRET&travisBuildId=$TRAVIS_BUILD_ID"
    exit 1;
 fi
 
