24d279b437bdd3d7c630863ef66da26e585c3334
Thomas Steur <thomas.steur@gmail.com>
2013-09-26 16:36:02 +1200

    use environment variables

--- travis.sh
+++ travis.sh
@@ -7,14 +7,14 @@ HAS_FATAL=$?
 
 if [ 0 == $HAS_FATAL ]; then
    echo "FATAL";
-   curl http://plugins.piwik/post-tests-hook/2.0.0/Evil/1.0.1/fatal?secret="$API_HOOK_SECRET"
+   curl "http://plugins.piwik.org/post-tests-hook/$TEST_PIWIK_VERISON/$TEST_PLUGIN_NAME/$TEST_PLUGIN_VERSION/fatal?secret=$API_HOOK_SECRET"
    exit 1;
 elif [ 0 == $BREAKS_BUILD ]; then
    echo "SUCCESS"
-   curl http://plugins.piwik/post-tests-hook/2.0.0/Evil/1.0.1/success?secret="$API_HOOK_SECRET"
+   curl "http://plugins.piwik.org/post-tests-hook/$TEST_PIWIK_VERISON/$TEST_PLUGIN_NAME/$TEST_PLUGIN_VERSION/success?secret=$API_HOOK_SECRET"
    exit 0;
 else
    echo "FAILURE";
-   curl http://plugins.piwik/post-tests-hook/2.0.0/Evil/1.0.1/failure?secret="$API_HOOK_SECRET"
+   curl "http://plugins.piwik.org/post-tests-hook/$TEST_PIWIK_VERISON/$TEST_PLUGIN_NAME/$TEST_PLUGIN_VERSION/failure?secret="$API_HOOK_SECRET"
    exit 1;
 fi
