b0355bde5b2d7805c86f790807c47ac9aecf57b7
Thomas Steur <tsteur@users.noreply.github.com>
2013-09-27 10:41:38 +1200

    verify all three test runs

--- travis.sh
+++ travis.sh
@@ -3,13 +3,15 @@ echo "" > result.log
 
 TEST_SUITE=PluginTests
 ./travis.sh 2>&1 | tee -a result.log
+BREAKS_BUILD_PLUGIN_TESTS=${PIPESTATUS[0]}
 
 TEST_SUITE=CoreTests
 ./travis.sh 2>&1 | tee -a result.log
+BREAKS_BUILD_CORE_TESTS=${PIPESTATUS[0]}
 
 TEST_SUITE=IntegrationTests
 ./travis.sh 2>&1 | tee -a result.log
-BREAKS_BUILD=${PIPESTATUS[0]}
+BREAKS_BUILD_INTEGRATION_TESTS=${PIPESTATUS[0]}
 
 cat result.log | grep "an unexpected response"
 HAS_FATAL=$?
@@ -18,7 +20,7 @@ if [ 0 == $HAS_FATAL ]; then
    echo "FATAL";
    curl "http://plugins.piwik.org/post-tests-hook/$TEST_PIWIK_VERISON/$TEST_PLUGIN_NAME/$TEST_PLUGIN_VERSION/fatal?secret=$API_HOOK_SECRET"
    exit 1;
-elif [ 0 == $BREAKS_BUILD ]; then
+elif [ 0 == $BREAKS_BUILD_PLUGIN_TESTS ] && [ 0 == $BREAKS_BUILD_CORE_TESTS ] && [ 0 == $BREAKS_BUILD_INTEGRATION_TESTS ]; then
    echo "SUCCESS"
    curl "http://plugins.piwik.org/post-tests-hook/$TEST_PIWIK_VERISON/$TEST_PLUGIN_NAME/$TEST_PLUGIN_VERSION/success?secret=$API_HOOK_SECRET"
    exit 0;
