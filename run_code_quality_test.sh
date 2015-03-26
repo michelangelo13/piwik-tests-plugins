57d70c0b44ffa24fb0de1a42ea0df9a1c3bdbd7c
diosmosis <benaka@piwik.pro>
2015-03-26 03:19:38 -0700

    Use PHP runner app packaged w/ PiwikStorm to run and upload inspection results instead of bash code.

--- run_code_quality_test.sh
+++ run_code_quality_test.sh
@@ -27,44 +27,10 @@ cp -R ../travis/resources/piwik.idea "$PIWIK_ROOT_DIR/.idea"
 # copy phpstorm vmoptions to phpstorm bin
 cp ../travis/resources/phpstorm/phpstorm64.vmoptions ./bin/phpstorm64.vmoptions
 
-echo ""
-echo "Running code inspection..."
-echo ""
-
 # run inspections
-cd piwikstorm/misc
-mkdir output
+cd piwikstorm/misc/runner
 
-travis_wait `pwd`/run_inspect.sh "`pwd`/../../" "$PIWIK_ROOT_DIR" "$TEST_PLUGIN_NAME" &> ./phpstorm.out # NOTE: `pwd` is important, inspect.sh requires absolute paths
-INSPECTION_RC=$?
+composer install &> /dev/null
 
-echo ""
-echo ""
-
-if [ "$(ls -A ./output)" ]; then
-    # print inspections in sort of pretty format
-    php "$TRAVIS_BUILD_DIR/travis/print_inspection_output.php"
-
-    # upload inspections as artifacts
-    echo ""
-    echo "Uploading inspection output..."
-    echo ""
-
-    cp phpstorm.out ./output
-
-    url_base="http://builds-artifacts.piwik.org/upload.php?auth_key=$ARTIFACTS_PASS&build_id=$TRAVIS_JOB_NUMBER&branch=inspections.$TEST_PLUGIN_NAME.$TEST_PLUGIN_VERSION"
-    if [ "$UNPROTECTED_ARTIFACTS" = "" ];
-    then
-        url_base="$url_base&protected=1"
-        using_protected=1
-    fi
-
-    tar -cjf inspections.tar.bz2 output --exclude='.gitkeep'
-    curl -X POST --data-binary @inspections.tar.bz2 "$url_base&artifact_name=inspections"
-
-    if [ "$(ls -A ./output/PiwikNonApiInspection.xml ./output/PhpDeprecationInspection.xml ./output/PhpUndefinedMethodInspection.xml )" ]; then
-        exit 1
-    fi
-else
-    echo "No output found, inspections pass!"
-fi
+# TODO: should use unprotected artifacts if .travis.yml of plugin contains UNPROTECTED_ARTIFACTS=1
+travis_wait php ./main.php inspections:run --piwik-path="$PIWIK_ROOT_DIR" --phpstorm-path="../../../" "$TEST_PLUGIN_NAME" --artifacts-pass="$ARTIFACTS_PASS" --upload-artifacts="$TRAVIS_JOB_NUMBER"
